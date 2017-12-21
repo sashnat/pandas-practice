2ce65b850a76e3359c7e92d1fa0016d429c2f75c
Nataliya Sashnikova <sashnat@yahoo.com>
2017-12-21 15:47:32 +0200

    pandas + functions updates

--- игра с начальником на отпуск.py
+++ игра с начальником на отпуск.py
@@ -1,25 +1,29 @@
 def me(ya):
     print("Я, " + ya + ", хочу в отпуск")
     def tr0():
-        q = input("Давай сыграем, если выиграешь, пойдешь в отпуск. Ok?:)")
+        q = input("Давай сыграем, если выиграешь, пойдешь в отпуск. Ok?:) ").lower()
         def tr():
            if q in s:
-                b = input("Как пишется слон на английском языке?")
+                b = input("Как пишется слон на английском языке?").lower()
                 i.append(str(1))
                 if b == "elephant":
                     print("Поздравляю! Ты уходишь в отпуск!" + \
                           " Осталось только написать заявление на отпуск." + \
                           " Форма здесь")
                     vac_form = r'C:\\Users\E277460\PycharmProjects\Jupyter\exercise_1\vac_form.xlsx'
-                    df = pd.read_excel('C:\\Users\E277460\PycharmProjects\Jupyter\exercise_1\\vac_form - Copy.xlsx')
-                    writer = pd.ExcelWriter(vac_form, engine='xlsxwriter')
-                    df.ix[0, 2] = ya if ya == "Natalya Sashnikova" else input("введите свое имя")
-                    df.ix[5, 2] = input("введите дату:")
-                    print(df)
-                    df.head(100)
+                    df_n = pd.read_excel(r'C:\\Users\E277460\PycharmProjects\Jupyter\exercise_1\\vac_form - Copy.xlsx')
 
+                    book = load_workbook(vac_form)
+                    writer = pd.ExcelWriter(vac_form, engine='openpyxl') # writes into a new sheet
+                    writer.book = book
 
-                    df.to_excel(writer, sheet_name='New', index=None)
+                    df_n.ix[0, 2] = ya if ya == "Natalya Sashnikova" else input("введите свое имя: ")
+                    df_n.ix[5, 2] = input("введите дату: ")
+
+                    print(df_n)
+                    df_n.head(100)
+
+                    df_n.to_excel(writer, sheet_name = "New", index = False )
                     writer.save()
                     print ("Хорошего отпуска !!!! ")
 
@@ -49,9 +53,8 @@ from openpyxl import load_workbook
 import os
 
 i = []
-s = ['ok', 'Ok', 'Yes', 'yes']
+s = ['ok', 'yes', 'sure', 'yeap']
 me(input("what is your name?"))
-
 ''' 
 ----------------------Output-----------------------------------
 
@@ -73,5 +76,5 @@ what is your name?m
 Хорошего отпуска !!!! 
 '''
 
-""
+
 
