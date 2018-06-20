a27abfa34915aea9e77d6e53936690430263a316
Nataliya Sashnikova <sashnat@yahoo.com>
2018-06-20 11:05:38 +0300

    pandas + function app

---
+++ taxes application old/taxes_add.py
@@ -0,0 +1,12 @@
+# for calculating taxes
+# functions + pandas
+# coded by Nataliya Sashnikova
+
+def income(x):
+    print ('tax, $:', z/100 * x/y)
+    return x/y - z/100 * x/y
+
+
+y = float(input('input currency exchange rate: '))
+z = float(input('input tax: '))
+
